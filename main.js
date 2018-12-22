a7880cfde2b33e6fe6365e443c55be021dc6c6eb
damjanpavlica <44097267+damjanpavlica@users.noreply.github.com>
2018-12-22 13:09:48 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+// https://api.lyrics.ovh/suggest/fraza
+const izvodjacInput = document.getElementById('izvodjac')
+const pesmaInput = document.getElementById('pesma')
+const dugmePotrazi = document.getElementById('potrazi')
+const rezultatElement = document.getElementById('rezultat')
+const naslovElement = document.getElementById('naslov')
+
+function potrazi() {
+  const izvodjac = izvodjacInput.value
+  const pesma = pesmaInput.value
+  const url = `https://api.lyrics.ovh/v1/${izvodjac}/${pesma}`
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      naslovElement.innerText = izvodjac + ' - ' + pesma
+      rezultatElement.innerText = data.lyrics
+    })
+}
+
+dugmePotrazi.addEventListener('click', potrazi)
